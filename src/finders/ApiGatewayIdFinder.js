c229893aae30237594212421078efe06d23ba25c
Steve Perkins <steve.perkins@ip3d.com>
2020-11-24 08:30:32 -0600

    API Gateway ID finder now does a slightly better job of importing

--- src/finders/ApiGatewayIdFinder.js
+++ src/finders/ApiGatewayIdFinder.js
@@ -13,15 +13,16 @@ class ApiGatewayIdFinder {
     }
     
     const getApiGatewayPaths = async function (apiGatewayId) {
-      const paths = {}
+      const pathsMap = {}
+
       const response = await provider.request("APIGateway", "getResources", { limit: 500, restApiId: apiGatewayId })
       if (response.items) {
         for (let resource of response.items) {
-          paths[resource.path] = resource.id
+          pathsMap[resource.path] = resource.id
         }
       }
 
-      return paths
+      return pathsMap
     }
     
     const importApiGateway = async (apiGatewayId) => {
@@ -44,15 +45,43 @@ class ApiGatewayIdFinder {
         throw new Error(`No root resource matching ${config.rootPath} exists on API Gateway ${apiGatewayId}`)
       }
 
-      const existingPaths = Object.keys(paths)
-      const resourcesToImport = {}
-      for (let resourcePath of existingPaths) {
-        const resourceId = paths[resourcePath]
-        if (!resourceId) {
-          throw new Error(`No path ${resourcePath} found in API Gateway ${apiGatewayId}`)
+      // Serverless purports to natively support shared API Gateways. The documentation neglects to mention that both
+      // an apiGatewayId and a rootResourceId must be provided. And that none of the paths in any Serverless template
+      // can be substrings of the paths in the current template (otherwise you'll get a parent name conflict because
+      // CloudFormation isn't smart enough to recognize that you're not redeploying e.g. /company/{companyId}, you're
+      // just trying to deploy /company/{companyId}/users).
+      //
+      // To avoid the name conflict, you can specify restApiResources - a map of path:resourceId. Who's going to manually
+      // add that?! What good is a Serverless template if I have to specify the root resource ID, which will change in
+      // every environment?! Even when done according to the documentation, using restApiResources causes weird API
+      // Gateway behavior - the resources are added to the target stage, but DELETED from the stage-less root resource tree.
+      // On the next deployment, they're re-added to the stage-less root resource tree and continue to flip-flop on each deploy.
+      //
+      // So here we're doing the best we can to supply all of the configuration we can to Serverless so it does the least-harmful
+      // thing - actually deploying the endpoints in this Serverless template, but flip-flopping between adding and deleting
+      // them in the stage-less root resource tree.
+      const matchingResourcePaths = []
+      if (serverless.service.functions) { 
+        for (let [fname, lambda] of Object.entries(serverless.service.functions)) {
+          if (lambda.events) {
+            for (let event of lambda.events) {
+              // We only care about lambdas that define an API Gateway integration
+              if (event.http && event.http.path) {
+                // We have a new HTTP path that needs to be added to the gateway. Check to see if a similar path already exists. If so, add it.
+                for (let path of Object.keys(paths)) {
+                  if (event.http.path.startsWith(path)) {
+                    matchingResourcePaths.push(path)
+                  }
+                }
+              }
+            }
+          }
         }
+      }
 
-        resourcesToImport[resourcePath] = resourceId
+      const resourcesToImport = {}
+      for (let resourcePath of matchingResourcePaths) {
+        resourcesToImport[resourcePath] = paths[resourcePath]
       }
 
       serverless.service.provider.apiGateway.restApiId = apiGatewayId
@@ -61,22 +90,18 @@ class ApiGatewayIdFinder {
       serverless.cli.log(`Imported API Gateway (${JSON.stringify(serverless.service.provider.apiGateway)})`)
     }
 
-
-
-    if (!this.apiGatewayIds) {
-      // See for available functions https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/APIGateway.html
-      const response = await this.provider.request(
-        "APIGateway",
-        "getRestApis",
-        {
-          limit: "500"
-        }
-      )
-      if (response) {
-        this.apiGatewayIds = {}
-        for (const gateway of response.items) {
-          this.apiGatewayIds[gateway.name] = gateway.id
-        }
+    // See for available functions https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/APIGateway.html
+    const response = await this.provider.request(
+      "APIGateway",
+      "getRestApis",
+      {
+        limit: "500"
+      }
+    )
+    if (response) {
+      this.apiGatewayIds = {}
+      for (const gateway of response.items) {
+        this.apiGatewayIds[gateway.name] = gateway.id
       }
     }
 
